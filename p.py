4cf9cba53f940ef5aceabf3ebc770f31b0c52a55
gagafik-sus <gahaguga999@gmail.com>
2026-02-23 13:44:20 +0500

    Update p.py

--- p.py
+++ p.py
@@ -1,22 +1,26 @@
 from decimal import Decimal, getcontext
+import os
 
-digits = int(input("сколько чисел после запятой?\n"))
-
-getcontext().prec = digits + 5
+digits = int(input("Сколько знаков после запятой? "))
+getcontext().prec = digits + 10
 
 a = Decimal(1)
 b = Decimal(1) / Decimal(2).sqrt()
 t = Decimal(1) / Decimal(4)
 p = Decimal(1)
 
-for _ in range(20):
-    a_next = (a + b) / Decimal(2)
+spinner = ['-', '\\', '|', '/']
+
+for i in range(25):
+    a_next = (a + b) / 2
     b = (a * b).sqrt()
     t -= p * (a - a_next)**2
     a = a_next
     p *= 2
+    print(f"\rВычисление... {spinner[i % 4]}", end="")
 
 pi = ((a + b)**2) / (4 * t)
-
 pi_str = str(pi)[:digits + 2]
-print(f"p = {pi_str}")
+
+print(f"\n\nP = {pi_s
+                 tr}")
