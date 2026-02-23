3dbb86f1ae9f96ebee2d4e67dd927db72e2c1c1b
gagafik-sus <gahaguga999@gmail.com>
2026-02-23 13:46:58 +0500

    Update p.py

--- p.py
+++ p.py
@@ -9,17 +9,15 @@ b = Decimal(1) / Decimal(2).sqrt()
 t = Decimal(1) / Decimal(4)
 p = Decimal(1)
 
-spinner = ['-', '\\', '|', '/']
-
-for i in range(25):
+for _ in range(25):
     a_next = (a + b) / 2
     b = (a * b).sqrt()
     t -= p * (a - a_next)**2
     a = a_next
     p *= 2
-    print(f"\rВычисление... {spinner[i % 4]}", end="")
 
 pi = ((a + b)**2) / (4 * t)
 pi_str = str(pi)[:digits + 2]
 
-print(f"\n\nPi = {pi_str}")
+print(f"\nPi = {pi_st
+                r}")
