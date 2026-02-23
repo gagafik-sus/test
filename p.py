648fea5afbe351d1368fe5829c263a90d6620cf6
gagafik-sus <gahaguga999@gmail.com>
2026-02-23 19:34:55 +0500

    Update p.py

--- p.py
+++ p.py
@@ -1,7 +1,7 @@
 from decimal import Decimal, getcontext
+import sys
 
-digits = int(input("сколько чисел после запятой?\n"))
-
+digits = int(input("кол-ов символов?\n"))
 getcontext().prec = digits + 5
 
 a = Decimal(1)
@@ -9,14 +9,26 @@ b = Decimal(1) / Decimal(2).sqrt()
 t = Decimal(1) / Decimal(4)
 p = Decimal(1)
 
-for _ in range(20):
-    a_next = (a + b) / Decimal(2)
+iterations = 20
+
+for i in range(1, iterations + 1):
+    a_next = (a + b) / 2
     b = (a * b).sqrt()
     t -= p * (a - a_next)**2
     a = a_next
     p *= 2
 
+    length = 20
+    filled = int(length * i // iterations)
+    bar = "#" * filled + "-" * (length - filled)
+
+    sys.stdout.write(f"\rПрогресс: [{bar}] {i}%")
+    sys.stdout.flush()
+
 pi = ((a + b)**2) / (4 * t)
 
+sys.stdout.write("\r" + " " * 20 + "\r")
+sys.stdout.flush()
+
 pi_str = str(pi)[:digits + 2]
-print(f"p = {pi_str}")
+print(f"π = {pi_str}")
