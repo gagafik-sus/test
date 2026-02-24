0bf527900e4d418714fa42936917610b27a8e7a5
gagafik-sus <gahaguga999@gmail.com>
2026-02-24 15:31:49 +0500

    Update p.py

--- p.py
+++ p.py
@@ -1,15 +1,16 @@
 from decimal import Decimal, getcontext
 import sys
+import math
 
-digits = int(input("кол-ов символов?\n"))
-getcontext().prec = digits + 5
+digits = int(input("Сколько знаков Пи вычислить?\n"))
+getcontext().prec = digits + 10 
+
+iterations = math.ceil(math.log2(digits)) + 1
 
 a = Decimal(1)
 b = Decimal(1) / Decimal(2).sqrt()
 t = Decimal(1) / Decimal(4)
 p = Decimal(1)
-
-iterations = 20
 
 for i in range(1, iterations + 1):
     a_next = (a + b) / 2
@@ -18,17 +19,13 @@ for i in range(1, iterations + 1):
     a = a_next
     p *= 2
 
-    length = 20
-    filled = int(length * i // iterations)
-    bar = "#" * filled + "-" * (length - filled)
-
-    sys.stdout.write(f"\rПрогресс: [{bar}] {i}/20")
+    fill = int(20 * i // iterations)
+    sys.stdout.write(f"\rПрогресс: [{'#'*fill}{'-'*(20-fill)}] {i}/{iterations}")
     sys.stdout.flush()
 
 pi = ((a + b)**2) / (4 * t)
-
-sys.stdout.write("\r" + " " * 20 + "\r")
-sys.stdout.flush()
-
 pi_str = str(pi)[:digits + 2]
-print(f"π = {pi_str}")
+
+sys.stdout.write("\n")
+print(p
+      i_str)
